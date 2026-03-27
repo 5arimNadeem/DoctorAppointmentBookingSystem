8eaae2a8cc5a844e43be3ddca9dcc92036426244
5arimNadeem <sarimnadeem141@gmail.com>
2026-03-28 01:08:41 +0500

    Contact.jsx and About.jsx page creation

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const About = () => {
+  const { assets } = useContext(AppContext)
   return (
-    <div>About</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+
+      <div className='flex flex-col my-10 md:flex-row gap-12'>
+        <img
+          className='w-full md:max-w-[360px]'
+          src={assets.about_image} alt="" />
+        <div className='flex flex-col my-10 gap-6 justify-center md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores iusto ratione voluptates modi qui hic illo sunt, unde consequatur facilis mollitia quo. Accusamus, officiis doloribus? At, officiis fugiat? Consequuntur, mollitia.</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores iusto ratione voluptates modi qui hic illo sunt, unde consequatur facilis mollitia quo. Accusamus, officiis doloribus? At, officiis fugiat? Consequuntur, mollitia.</p>
+          <b className='text-gray-900'>Our Vision</b>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores iusto ratione voluptates modi qui hic illo sunt, unde consequatur facilis mollitia quo. Accusamus, officiis doloribus? At, officiis fugiat? Consequuntur, mollitia.</p>
+        </div>
+      </div>
+      <div className='text-xl my-4'>
+        <p className=' text-gray-500'>WHY <span className='text-gray-900 font-semibold'>CHOOSE US</span></p>
+      </div>
+
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all text-gray-500 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, consequuntur.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all text-gray-500 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, consequuntur.</p>
+
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all text-gray-500 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, consequuntur.</p>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Contact = () => {
+  const { assets } = useContext(AppContext)
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p className=' text-gray-500'>CONTACT <span className='text-gray-900 font-semibold'>US</span></p>
+      </div>
+
+      <div className='flex
+
+        flex-col my-10 justify-center md:flex-row gap-10 mb-28 text-sm'>
+
+        <img
+          className='md:max-w-[560px]'
+
+          src={assets.contact_image} alt="" />
+
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className=' text-gray-500 text-2xl'>OUR OFFICE</p>
+          <p>Lorem ipsum <br /> dolor sit amet.</p>
+          <p>Tel: (432) 888-0132 <br /> Email: sarim@gmail.com</p>
+          <p className=' text-gray-500 text-2xl'>CAREERS AT PERSCRIPTO</p>
+          <p>Lorem ipsum dolor sit amet consectetur.</p>
+          <button className='border border-black px-8 p-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
